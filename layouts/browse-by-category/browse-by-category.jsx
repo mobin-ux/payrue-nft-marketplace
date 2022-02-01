3fdb987ef2211b64391b37abc286ebf89d82ae7e
mobin-ux <mobin.gh505@gmail.com>
2022-02-02 00:45:31 +0330

    update

---
+++ layouts/browse-by-category/browse-by-category.jsx
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import css from "./browse-by-category.module.scss";
+import { items } from "./data";
+
+const ItemStyle = styled.div`
+  background-image: url(${(props) => props.backgroundImage});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: ${(props) => (props.isLarg ? "586px" : "285px")};
+  @media screen and (max-width: 768px) {
+    height: 285px;
+  }
+  div {
+    border: 4px solid #ffffff;
+    backdrop-filter: blur(14px);
+  }
+`;
+const BrowseByCategory = ({ properties, categories }) => {
+  return (
+    <div
+      className={`lg:pt-16 mb-64 mx-auto lg:mx-0 flex justify-center flex-col ${css.container}`}
+    >
+      <div className={`relative ${css.container}`}>
+        <img
+          src="/img/browse-by-category-line-yellow.png"
+          className={`absolute z-10 top-0 ${css.yellowLine}`}
+          alt=""
+        />
+        <img
+          src="/img/browse-by-category-line-blue.png"
+          className={`absolute z-10 ${css.blueLine} bottom-0`}
+          alt=""
+        />
+        {categories.length > 0 && (
+          <h2 className="text-payrue-black font-medium text-4xl text-center mb-12">
+            Browse by category
+          </h2>
+        )}
+        <div className="grid grid-rows-4 grid-cols-2 md:grid-rows-3 md:grid-cols-3 relative  gap-4 z-20">
+          {items.map((el, idx) => (
+            <div className={el?.class} key={idx}>
+              <ItemStyle
+                className="w-full flex items-end"
+                backgroundImage={el.image}
+                isLarg={el.isLarg}
+              >
+                <div className="w-full py-5 text-center text-payrue-home-blue font-semibold text-3xl">
+                  {el.title}
+                </div>
+              </ItemStyle>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { BrowseByCategory };
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+  purge: [
+    './components/**/*.js',
+    './components/**/*.jsx',
+    './pages/**/*.js',
+    './pages/**/*.jsx',
+    './layouts/**/*.js',
+    './layouts/**/*.jsx'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["poppins"],
+    },
+    extend: {
+      colors: {
+        "payrue-blue": "#4594f7",
+        "payrue-home-blue": "#0050FF",
+        "payrue-blue-6": "#4594F705",
+        "payrue-black": "#373F51",
+        "payrue-gray": "#373F5166",
+        "payrue-gray-60": "#373F5166",
+      },
+      height: {
+        144: "36rem",
+        175: "43rem",
+      },
+      width: {
+        116: "29rem",
+        144: "36rem",
+        175: "43rem",
+      },
+      padding: {
+        108: "27rem",
+      },
+      fontFamily: {
+        poppins: ["poppins"],
+      },
+      transitionProperty: {
+        height: "height",
+        spacing: "margin, padding",
+      },
+      flex: {
+        0.7: "0.7",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      borderRadius: ["hover", "focus"],
+    },
+  },
+  plugins: [],
+};
